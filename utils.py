4712575407bdd327c0b5447430f47f80f671cb21
ylilaa <y.lila@live.fr>
2022-07-14 12:58:55 +0100

    better table refresh

--- utils.py
+++ utils.py
@@ -12,6 +12,12 @@ from config import *
 def donothing():
     messagebox.showinfo(title="walo", message=" w ta 9lwa walo")
 
+def initial_fund_display():
+    config = AppConfig()
+    original_df = pd.read_excel(config.db_name)
+    print(colored('Was here','red'))
+    return original_df.iloc[0]['CODE_FONDS']
+
 def get_fund_by_name(fund):
     config = AppConfig()
     original_df = pd.read_excel(config.db_name)
@@ -25,6 +31,7 @@ def get_funds_array():
     array = original_df['CODE_FONDS'].unique()
     print(colored('List of Funds retrieved from database Successfully','green'))
     return array
+
 
 
 
